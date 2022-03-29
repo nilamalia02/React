f34d89f09c3f15ca930a00aaaf23e61d41bcc3f2
Tharixs Akbar <tharixsakbar@gmail.com>
2022-03-29 20:07:50 +0700

    add component

---
+++ speakup/src/page/landing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Landing Page</h1>
+        <p>This is the landing page</p>
+      </div>
+    );
+  }
+}
+
+export default Landing;
