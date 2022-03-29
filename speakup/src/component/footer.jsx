22bbc4868ba8f63c307305482f09d133254fac2e
alfian_ezza <rianezza86@gmail.com>
2022-03-29 23:30:57 +0700

    Footer

--- speakup/src/component/footer.jsx
+++ speakup/src/component/footer.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faInstagram, faYoutube, faFacebookSquare} from '@fortawesome/free-brands-svg-icons';
+
+import "./footer.css";
 
 const Footer = () => {
   return(
-      <div className="container">
+      <div name="footer" id="footer" className="container-fluid">
         <footer className="py-5">
           <div className="row">
-            <div className="col-4 ">
-              <h4>SpeakUp</h4>
-              <h5>Counseling App</h5>
-              <p>We will help you to overcome mental health problems and help you to reach your goals to make your life more meaningful</p>
+            <div className="col-4 offset-1">
+              <h4 id="footer-app-title">SpeakUp</h4>
+              <p id="footer-app-subtitle">Counseling App</p>
+              <p id="footer-app-desc">We will help you to overcome mental health problems and help you to reach your goals to make your life more meaningful</p>
+              <FontAwesomeIcon icon={faInstagram} id="ig"/>
+              <FontAwesomeIcon icon={faYoutube} className="offset-1" id="yt"/>
+              <FontAwesomeIcon icon={faFacebookSquare} className="offset-1" id="fb"/>
             </div>
-            <div className="col-2">
-            <h4>Company</h4>
+            <div className="col-1 offset-2">
+            <h4 id="company">Company</h4>
               <ul>
               <li className="nav-item mb-2">
                 <a href="#" className="nav-link p-0 text-muted">Home</a>
@@ -24,8 +31,8 @@ const Footer = () => {
               </li>
               </ul>
             </div>
-            <div className="col-2">
-              <h4>Policies</h4>
+            <div className="col-1">
+              <h4 id="policy">Policies</h4>
               <ul>
               <li className="nav-item mb-2">
                 <a href="#" className="nav-link p-0 text-muted">Terms</a>
@@ -39,7 +46,7 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col-3">
-              <h4>Contacts</h4>
+              <h4 id="contacts">Contacts</h4>
               <ul>
               <li className="nav-item mb-2">
                 <a href="#" className="nav-link p-0 text-muted">+62 812 3568 6798</a>
