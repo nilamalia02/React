92eb759ae29b508cdbe591e6535e19fd3b2f18e7
nilamalia02 <nilaa3794@gmail.com>
2022-03-30 07:56:19 +0700

    nila

--- speakup/src/component/navbar.jsx
+++ speakup/src/component/navbar.jsx
@@ -1,8 +1,36 @@
+
 import React from "react";
-import navbar from "./navbar.css";
+import speakup from "../assets/speakup.jpg";
+import "./navbar.css";
+// import { unstable_batchedUpdates } from "react-dom";
+import { useState } from "react";
 
 const Navbar = () => {
-  return;
+  const [nav, setnav] = useState(false);
+  const changeBackground = () => {
+    if(window.scrollY >= 50) {
+      setnav(true);
+    }
+    else {
+      setnav(false);
+    }
+  }
+  window.addEventListener("scroll", changeBackground);
+  return(
+    <div className="container-fluid">
+      <header className="nav">
+      <a className="img">
+        <img src={speakup}/>
+      </a>
+      <ul className="menu">
+        <h2 href="#">Home</h2>
+        <h2 href="#">Services</h2>
+        <h2 href="#">Diary</h2>
+        </ul>
+        <button className="button">Login</button>
+        </header>
+    </div>
+  )
 };
 
 export default Navbar;
